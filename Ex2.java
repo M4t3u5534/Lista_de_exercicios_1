9daa90b6df0bd5e01f593d8fef60eef4dfd24790
Mateus <mateusrcerqueira@gmail.com>
2025-02-24 12:27:57 -0300

    Ex2

--- Ex2.java
+++ Ex2.java
@@ -4,17 +4,22 @@ import java.util.Scanner;
 public class Ex2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String vogais[] = {"A","E","I","O","U"};
-        System.out.println("Digite uma letra:");
-        String vogal = scanner.next();
-        for(int i=0;i<5;i++){
-            if(vogal.equalsIgnoreCase(vogais[i])){
-            System.out.println("A letra digitada é uma vogal.");
-            scanner.close();
-            return;
+        try {
+            String vogais[] = {"A","E","I","O","U"};
+            System.out.println("Digite uma letra:");
+            String vogal = scanner.next();
+            for(int i=0;i<5;i++){
+                if(vogal.equalsIgnoreCase(vogais[i])){
+                System.out.println("A letra digitada é uma vogal.");
+                scanner.close();
+                return;
+                }
             }
+            System.out.println("A letra digitada é uma consoante.");
+            scanner.close();
+        } catch (InputMismatchException e) {
+            System.out.println("Você deve digitar uma letra!");
         }
-        System.out.println("A letra digitada é uma consoante.");
-        scanner.close();
+        
     }
 }
