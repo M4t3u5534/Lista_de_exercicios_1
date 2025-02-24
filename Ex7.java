1e64b7d674b0bb6f41d448c86c2493f19cfe14c0
Mateus <mateusrcerqueira@gmail.com>
2025-02-24 14:57:43 -0300

    inicio Ex7

---
+++ Ex7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ex7 {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String vogais[] = {"A","E","I","O","U"};
+    System.out.println("Digite uma fase:");
+    String vogal = scanner.nextLine();
+    while () {
+        if(vogal.equalsIgnoreCase(vogais[i])){
+            System.out.println("A letra digitada é uma vogal.");
+        }
+    }
+    scanner.close();
+    }
+}
