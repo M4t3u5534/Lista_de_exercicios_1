262e89a59c53f3b0446e0e9b8857b304aefd4393
Mateus <mateusrcerqueira@gmail.com>
2025-02-25 16:16:21 -0300

    Ex7

--- Ex7.java
+++ Ex7.java
@@ -1,16 +1,27 @@
 import java.util.Scanner;
 
 public class Ex7 {
+    public static int contarVogais(String frase) {
+        int quantidadeVogais = 0;
+        String vogais[] = {"A","E","I","O","U"};
+        String[] texto = frase.toUpperCase().split("");
+        int tamanho_texto = texto.length;
+
+        for(int i=0;i<tamanho_texto;i++){
+            for(int j=0;j<5;j++){
+                if(texto[i].equals(vogais[j])){
+                    quantidadeVogais++;
+                    continue;
+                }
+            }
+        }
+        return quantidadeVogais;
+    }
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    String vogais[] = {"A","E","I","O","U"};
     System.out.println("Digite uma fase:");
-    String vogal = scanner.nextLine();
-    while () {
-        if(vogal.equalsIgnoreCase(vogais[i])){
-            System.out.println("A letra digitada é uma vogal.");
-        }
-    }
+    String frase = scanner.nextLine();
+    System.out.println("A frase tem " + contarVogais(frase) + " vogais");
     scanner.close();
     }
 }
